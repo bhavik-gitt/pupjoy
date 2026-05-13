f0c01b8684a9902434ad9eb74b1a4b6b9108d326
bhavik-gitt <bhavikgithub@gmail.com>
2026-05-13 13:13:08 +0530

    fix: update title in index.html for branding consistency and simplify author name in Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,11 @@ export default function Footer() {
   <footer className="text-center py-4 text-muted mt-auto">
   <small>
     © {new Date().getFullYear()} <strong>Pupjoy</strong> — <em>Your Daily Dose of Dog Joy 🐾</em><br />
-    Created with ❤️ by <strong>Bhavik Ratnottar</strong> using{" "}
+    Created with ❤️ by <strong>Bhavik</strong> using{" "}
     <a href="https://dog.ceo/dog-api/" className="text-black" style={{ textDecoration: 'none' }} target="_blank" rel="noopener noreferrer">
       Dog CEO API
     </a>
     .<br />
-    {/* Enjoy real-time updates of the cutest dog images from around the world. */}
   </small>
 </footer>
 
